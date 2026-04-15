45fea5445e9133b56f070e955bbd75b5e5b11c60
DellaBitta <drsanta@google.com>
2026-04-15 10:31:22 -0400

    config

--- ai/ai-react-app/src/config/firebase-config.ts
+++ ai/ai-react-app/src/config/firebase-config.ts
@@ -1,11 +1,9 @@
 // TODO (developer): Replace with your Firebase project configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyAjKXaQzt-AvfPwUDuBVV3dyfQRP-RP9L8",
-  authDomain: "jscore-sandbox-141b5.firebaseapp.com",
-  databaseURL: "https://jscore-sandbox-141b5.firebaseio.com",
-  projectId: "jscore-sandbox-141b5",
-  storageBucket: "jscore-sandbox-141b5.appspot.com",
-  messagingSenderId: "280127633210",
-  appId: "1:280127633210:web:3f579f142972038b46c203",
-  measurementId: "G-YRDJ8EMX95"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
